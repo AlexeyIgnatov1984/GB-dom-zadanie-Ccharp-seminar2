20d9805acb8427994c6a2ae09ad60b9fe7f994bf
aleksey <ignatulya@mail.ru>
2022-06-04 12:37:02 +0300

    Task 10

---
+++ Task_10/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 10. Напишите программу, которая на вход принимает трехзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число:   ");
+string numberStr = Console.ReadLine();
+int number = int.Parse(numberStr);
+string strr = number.ToString();
+Console.WriteLine($"{number} -> {strr[1]}");
